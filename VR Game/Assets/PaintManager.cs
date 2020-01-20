e7cd8f951521040e21f6f908cb33602cb214576a
DavidSalzmann <50995334+DavidSalzmann@users.noreply.github.com>
2020-01-19 19:18:32 -0800

    PaintingAdded! Made our Base Scene the Interactables example

---
+++ VR Game/Assets/PaintManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintManager : MonoBehaviour
+{
+    
+    public Transform Paint;
+    private Rigidbody rb;
+    private float moveZAxis = 0;
+    private int timer = 0;
+    private float wallDepth;
+    private float brushPosY;
+    private float brushPosX;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        brushPosY = GameObject.Find("PaintBrush").transform.position.y;
+        wallDepth = GameObject.Find("PaintWallHitBox").transform.position.z;
+        brushPosX = GameObject.Find("PaintBrush").transform.position.x;
+
+    }
+
+    void FixedUpdate()
+    {
+        brushPosY = GameObject.Find("PaintBrush").transform.position.y;
+        brushPosX = GameObject.Find("PaintBrush").transform.position.x;
+       
+
+    }
+
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("PaintWallHitBox") && timer >= 5)
+        {
+            Instantiate(Paint, new Vector3(brushPosX, brushPosY, wallDepth), Quaternion.identity);
+            
+            timer = 0;
+        }
+        timer += 1;
+
+    }
+}
