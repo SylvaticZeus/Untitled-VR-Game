42ff2bbf8c2a425b99f6c70332d0ce37409fbc8c
SylvaticZeus <54227852+SylvaticZeus@users.noreply.github.com>
2020-01-21 07:57:38 -0800

    did things
    s

---
+++ VR Game/Assets/floatup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatup : MonoBehaviour
+{
+    public float floatStrength = 3.5f;
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * floatStrength);
+    }
+}
---
+++ VR Game/Assets/makebaloon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class makebaloon : MonoBehaviour
+{
+    public GameObject baloon;
+    public Rigidbody rb;
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonUp(0)) {
+            GameObject clone;
+            clone = Instantiate(baloon);
+            rb = clone.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+        }
+    }
+}
